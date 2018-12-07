a08dc0d522d68672937a32f73fd254226cc66a51
Kernael92 <kernaelapuko5@gmail.com>
2018-12-07 11:16:47 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kernaeljoy/Documents/Moringa-School-Projects/python/Django/gallery/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
