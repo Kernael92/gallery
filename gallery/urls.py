1d5feefa57b7a0411b900a49061ac6dccbc83ff8
Kernael92 <kernaelapuko5@gmail.com>
2018-12-09 00:20:05 +0300

    install pillow so as to run the image field

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
     url(r'^today/$',views.gallery_today,name = 'galleryToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})$',views.past_days_gallery,name = 'pastGallery')
-    
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
