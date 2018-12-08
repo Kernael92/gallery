1129b059ac3c035506129b26b5d33055f0ac1438
Kernael92 <kernaelapuko5@gmail.com>
2018-12-09 00:47:01 +0300

    create a category and location model classes as well as one to many relationship between the image and category, location classes

--- gallery/models.py
+++ gallery/models.py
@@ -1,8 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name =models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name 
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 245)
+
+    def __str__(self):
+        return self.name 
+
+
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length = 30)
-    image_description = models.CharField(max_length = 254)
-    
+    image_description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image
+    class Meta:
+        ordering = ['image']
+
+
+
+
+
