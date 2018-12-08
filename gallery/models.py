a606bbdc952c2dae8448ee54a532bf734665f610
Kernael92 <kernaelapuko5@gmail.com>
2018-12-09 00:26:42 +0300

    create an Image class model and run migrations

--- gallery/models.py
+++ gallery/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length = 30)
+    image_description = models.CharField(max_length = 254)
+    
